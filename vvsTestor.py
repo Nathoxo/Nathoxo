73ec84a3be5911f8a8f8aef8e24db399836e92e7
Nathoxo <nathan.peyaud@gmail.com>
2024-06-04 15:28:43 +0200

    Create vvsTestor.py

---
+++ vvsTestor.py
@@ -0,0 +1,39 @@
+import os
+import datetime
+import random
+import shutil
+import re
+import argparse
+
+def lister_dossiers():
+    chemin = "C:\\vvsTestor\\conf_sit"
+    dossiers = []
+    listdir = os.listdir(chemin)
+    for dossier in listdir:
+        if dossier.startswith('test-case_'):
+            dossiers.append(dossier)
+
+    dossiers.sort()
+
+    for dossier in dossiers:
+        print(dossier)
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    #creation  des paramètres attendu
+    parser.add_argument("-l", "--lister", help="Afficher la liste des dossiers", action="store_true")
+    parser.add_argument("-C", "--Create", help="Afficher la liste des dossiers", action="store_true")
+
+    #analyse des paramètre trouvé
+    args = parser.parse_args()
+
+    if args.lister:
+        lister_dossiers()
+
+    if args.Create:
+        print("print")
+
+if __name__ == "__main__":
+    main()
+
